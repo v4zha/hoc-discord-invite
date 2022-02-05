fbf4b20390a6fb7dfc4c5e8327ccdf4b20e80a97
v4zha <arvin_j4848@protonmail.com>
2022-02-05 10:05:58 +0530

    build

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const serveStatic = require('serve-static');
 const path=require('path');
 const app=express();
 app.use(serveStatic(path.join(__dirname,'dist')));
-const port=process.env.port||80;
+const port=process.env.PORT;
 app.listen(port);
 console.log("server started running on "+port);
